8c1af48a975eb39d02bec2d5e0050fa826c9a8bb
Yahya Wahbeh <outstandingapp@gmail.com>
2016-03-15 18:40:51 -0400

    updated outstanding before symp

--- js/src.js
+++ js/src.js
@@ -30,33 +30,14 @@ $('.typeit-box_title').typeIt({
 });
 
 
-$('.typeit-box').typeIt({
-
-  // what to type
-  whatToType:['Sitting is the new smoking.', 'Not taking enough breaks from sitting can cause:', 'Heart Disease', 'Diabetes', 'Cancer', 'Obesity', 'High Blood Pressure', 'Muscle Degeneration'],
-
-  // typing speed
-  typeSpeed: 50,
-
-  // make the typing pace irregular
-  lifeLike: false,
-
-  // show cursor
-  showCursor: true,
-
-  // Choose whether you want multiple strings to be printed on top of each other
-  breakLines: true,
-
-  // The amount of time between typing multiple strings.
-  breakDelay: 750,
-
-  // The amount of time before the plugin begins typing after initalizing.
-  startDelay: 250,
-
-  // Have your string or strings continuously loop after completing.
-  loop: true,
-
-  // The amount of time between looping over a string or set of strings again.  
-  loopDelay: 2000
-  
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
 });
+
+
+
